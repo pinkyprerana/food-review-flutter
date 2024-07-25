5ea41fe61dc00006a32f30679b0b0dd7cd7aecdb
Pinky Prerana <pinkyprerna99@gmail.com>
2024-07-25 17:37:45 +0530

    setState

--- lib/widgets/custom_input_field.dart
+++ lib/widgets/custom_input_field.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:for_the_table/core/styles/app_colors.dart';
 import 'package:for_the_table/core/styles/app_text_styles.dart';
-import '../screens/auth/shared/providers.dart';
 
-class CustomInputField extends ConsumerStatefulWidget {
+class CustomInputField extends StatefulWidget {
   const CustomInputField({
     this.label,
     required this.hint,
@@ -18,6 +16,7 @@ class CustomInputField extends ConsumerStatefulWidget {
     this.onFieldSubmitted,
     super.key,
   });
+
   final String? label;
   final String hint;
   final bool isPassword;
@@ -29,14 +28,14 @@ class CustomInputField extends ConsumerStatefulWidget {
   final Function(String)? onFieldSubmitted;
 
   @override
-  ConsumerState<CustomInputField> createState() => _CustomInputFieldState();
+  State<CustomInputField> createState() => _CustomInputFieldState();
 }
 
-class _CustomInputFieldState extends ConsumerState<CustomInputField> {
+class _CustomInputFieldState extends State<CustomInputField> {
+  bool isPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
-    final state = ref.watch(authNotifierProvider);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: TextFormField(
@@ -47,7 +46,7 @@ class _CustomInputFieldState extends ConsumerState<CustomInputField> {
           filled: true,
           fillColor: AppColors.colorGrey,
           alignLabelWithHint: true,
-          labelText: (widget.label != null) ? widget.label : null,
+          labelText: widget.label,
           labelStyle: AppTextStyles.textStylePoppinsLight.copyWith(
             color: AppColors.colorPrimaryAlpha,
             fontSize: 11.sp,
@@ -58,29 +57,28 @@ class _CustomInputFieldState extends ConsumerState<CustomInputField> {
             color: AppColors.colorPrimaryAlpha,
           ),
           border: InputBorder.none,
-          floatingLabelBehavior:
-              (widget.label != null) ? FloatingLabelBehavior.always : FloatingLabelBehavior.never,
+          floatingLabelBehavior: widget.label != null
+              ? FloatingLabelBehavior.always
+              : FloatingLabelBehavior.never,
           suffixIcon: widget.isPassword
               ? GestureDetector(
-            onTap: () => ref.read(authNotifierProvider.notifier).togglePasswordVisibility(),
-            child: state.isPasswordVisible
-                      ? const Icon(
-                          Icons.visibility_off_outlined,
-                          color: AppColors.colorPrimaryAlpha,
-                        )
-                      : const Icon(
-                          Icons.visibility_outlined,
-                          color: AppColors.colorPrimaryAlpha,
-                        ),
-                )
+            onTap: () => setState(() {
+              isPasswordVisible = !isPasswordVisible;
+            }),
+            child: Icon(
+              isPasswordVisible
+                  ? Icons.visibility_off_outlined
+                  : Icons.visibility_outlined,
+              color: AppColors.colorPrimaryAlpha,
+            ),
+          )
               : const Icon(
-                  Icons.text_fields_rounded,
-                  color: AppColors.colorTransparent,
-                ),
-          // prefixText: widget.keyboardType == TextInputType.phone ? '+1 ' : null,
+            Icons.text_fields_rounded,
+            color: AppColors.colorTransparent,
+          ),
         ),
         keyboardType: widget.keyboardType,
-        obscureText: widget.isPassword && !state.isPasswordVisible,
+        obscureText: widget.isPassword && !isPasswordVisible,
         obscuringCharacter: '*',
         style: AppTextStyles.textStylePoppinsRegular.copyWith(fontSize: 13.sp),
         onFieldSubmitted: widget.onFieldSubmitted,
