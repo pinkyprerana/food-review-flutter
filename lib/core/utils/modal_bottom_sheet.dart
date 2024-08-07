8aa21d7b439a284f7a40a3618e6244b2ca825f87
Manish Paul <manish.paul@webskitters.in>
2024-08-07 12:55:45 +0530

    fix: closing keyboard on click outside TextField

--- lib/core/utils/modal_bottom_sheet.dart
+++ lib/core/utils/modal_bottom_sheet.dart
@@ -19,16 +19,19 @@ Future<void> commonModal(
           children: [
             Stack(
               children: [
-                Container(
-                  padding: const EdgeInsets.only(top: 25, bottom: 15, left: 10, right: 10).r,
-                  decoration: const BoxDecoration(
-                    color: AppColors.colorWhite,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15),
+                GestureDetector(
+                  onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+                  child: Container(
+                    padding: const EdgeInsets.only(top: 25, bottom: 15, left: 10, right: 10).r,
+                    decoration: const BoxDecoration(
+                      color: AppColors.colorWhite,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
+                      ),
                     ),
+                    child: child,
                   ),
-                  child: child,
                 ),
                 Positioned(
                   right: -1,
