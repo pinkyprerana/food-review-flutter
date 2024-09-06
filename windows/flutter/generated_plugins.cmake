8a4e8f32a146b8334dcaab3b8111ee254e456664
Pinky Prerana <pinkyprerna99@gmail.com>
2024-09-06 12:27:12 +0530

    pulled from fix-home-page-manish

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,7 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  awesome_notifications
   file_selector_windows
+  firebase_auth
+  firebase_core
+  flutter_secure_storage_windows
   geolocator_windows
   permission_handler_windows
   share_plus
