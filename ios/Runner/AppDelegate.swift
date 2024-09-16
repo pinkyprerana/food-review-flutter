6937d4f4430337d11f0b4b8283d4c9d2a2d4da98
Pinky Prerana <pinkyprerna99@gmail.com>
2024-09-16 19:40:44 +0530

    notification in ios release

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import GoogleMaps
+import Firebase
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,7 +10,8 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    GMSServices.provideAPIKey("AIzaSyDoHn9fLrBIKZ3bJ8LdJ1e0pcqTlDWpYxU") 
+    GMSServices.provideAPIKey("AIzaSyDoHn9fLrBIKZ3bJ8LdJ1e0pcqTlDWpYxU")
+    application.registerForRemoteNotifications()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
