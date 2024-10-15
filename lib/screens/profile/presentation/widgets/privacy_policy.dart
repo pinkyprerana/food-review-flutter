90ec3a4606516f17392d56c8c12841a59169d987
Pinky Prerana <pinkyprerna99@gmail.com>
2024-10-15 12:16:37 +0530

    standings search empty issue solved

--- lib/screens/profile/presentation/widgets/privacy_policy.dart
+++ lib/screens/profile/presentation/widgets/privacy_policy.dart
@@ -5,7 +5,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:for_the_table/screens/profile/shared/providers.dart';
 import '../../../../core/styles/app_colors.dart';
 import '../../../../core/styles/app_text_styles.dart';
-import '../../../../core/utils/app_log.dart';
 
 @RoutePage()
 class PrivacyPolicyPage extends ConsumerStatefulWidget {
@@ -29,7 +28,7 @@ class _PrivacyPolicyPageState extends ConsumerState<PrivacyPolicyPage> {
   @override
   Widget build(BuildContext context) {
     final profileState = ref.watch(profileNotifierProvider);
-    AppLog.log("Privacy Policy : ${profileState.privacyPolicy}");
+
     return Scaffold(
       extendBody: true,
       extendBodyBehindAppBar: false,
@@ -83,9 +82,10 @@ class _PrivacyPolicyPageState extends ConsumerState<PrivacyPolicyPage> {
                   ),
                 ),
                 10.verticalSpace,
-                Text(
+                profileState.privacyPolicy.isEmpty
+                ? const CircularProgressIndicator(color: AppColors.colorPrimary,)
+                : Text(
                   profileState.privacyPolicy,
-                  // "Lorem ipsum dolor sit amet consectetur. Et risus habitasse viverra duis sed arcu eget commodo. Et vitae leo sollicitudin id libero accumsan nullam. Pulvinar sit nunc viverra aliquam tincidunt feugiat. Tempus a tellus amet pulvinar feugiat vitae orci elit. Libero nisi mi egestas at arcu nec amet varius pharetra. Scelerisque pellentesque etiam sed tempus bibendum. Massa elementum proin tincidunt laoreet aliquet sed interdum sed. Sagittis cum ac volutpat curabitur lacus nibh eget augue. Imperdiet luctus commodo mauris amet aliquet malesuada. Rhoncus bibendum adipiscing laoreet diam ac suspendisse et vitae lorem. Sed mauris nulla nunc at cursus.\n\nEt dui tincidunt sollicitudin a non pulvinar. Auctor et metus auctor odio convallis metus vitae. Id imperdiet tincidunt tortor quisque.",
                   style: AppTextStyles.textStylePoppinsRegular.copyWith(
                     color: AppColors.colorPrimaryAlpha,
                     fontSize: 12.sp,
@@ -95,7 +95,7 @@ class _PrivacyPolicyPageState extends ConsumerState<PrivacyPolicyPage> {
                 Align(
                   alignment: Alignment.topLeft,
                   child: Text(
-                    "Terms And Consditions",
+                    "Terms And Conditions",
                     style: AppTextStyles.textStylePoppinsMedium.copyWith(
                       color: AppColors.colorBlack,
                       fontSize: 14.sp,
