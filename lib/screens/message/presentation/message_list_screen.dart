c7059191a8689d1e5ab8cf74d812b6121ae295c8
Pinky Prerana <pinkyprerna99@gmail.com>
2024-11-11 16:00:04 +0530

    FocusManager.instance.primaryFocus?.unfocus()

--- lib/screens/message/presentation/message_list_screen.dart
+++ lib/screens/message/presentation/message_list_screen.dart
@@ -40,180 +40,184 @@ class _MessageListScreenState extends ConsumerState<MessageListScreen> {
   Widget build(BuildContext context) {
     final state = ref.watch(chatNotifierProvider);
     final stateNotifier = ref.watch(chatNotifierProvider.notifier);
-    AppLog.log("Chat : ${state.allChatList}");
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        centerTitle: false,
-        automaticallyImplyLeading: false,
-        leading: GestureDetector(
-          onTap: () => Navigator.pop(context),
-          child: Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.only(top: 10.h, left: 20.w, right: 0, bottom: 10.h),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: AppColors.colorPrimary.withOpacity(0.20),
+    AppLog.log("Chat list : ${state.allChatList}");
+
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: false,
+          automaticallyImplyLeading: false,
+          leading: GestureDetector(
+            onTap: () => Navigator.pop(context),
+            child: Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 10.h, left: 20.w, right: 0, bottom: 10.h),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: AppColors.colorPrimary.withOpacity(0.20),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  5.horizontalSpace,
+                  Icon(Icons.arrow_back_ios, color: AppColors.colorPrimary, size: 15.h),
+                ],
+              ),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          title: Text(
+            'Messages',
+            style: AppTextStyles.textStylePoppinsBold.copyWith(
+              color: AppColors.colorPrimary,
+              fontSize: 16.sp,
+            ),
+          ),
+        ),
+        body: GestureDetector(
+          onTap: FocusScope.of(context).unfocus,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                5.horizontalSpace,
-                Icon(Icons.arrow_back_ios, color: AppColors.colorPrimary, size: 15.h),
-              ],
-            ),
-          ),
-        ),
-        title: Text(
-          'Messages',
-          style: AppTextStyles.textStylePoppinsBold.copyWith(
-            color: AppColors.colorPrimary,
-            fontSize: 16.sp,
-          ),
-        ),
-      ),
-      body: GestureDetector(
-        onTap: FocusScope.of(context).unfocus,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              16.verticalSpace,
-               CustomSearchField(
-                controller: stateNotifier.searchController,
-                 onChanged: (_) => stateNotifier.searchChat(),
-              ),
-              20.verticalSpace,
-              Text(
-                'Message List',
-                style: AppTextStyles.textStylePoppinsMedium.copyWith(
-                  color: AppColors.colorPrimary,
-                  fontSize: 13.sp,
+                16.verticalSpace,
+                 CustomSearchField(
+                  controller: stateNotifier.searchController,
+                   onChanged: (_) => stateNotifier.searchChat(),
                 ),
-              ),
-              4.verticalSpace,
-              Text(
-                state.allChatList.length.toString(),
-                style: AppTextStyles.textStylePoppinsRegular.copyWith(
-                  color: AppColors.colorPrimaryAlpha,
-                  fontSize: 10.sp,
-                ),
-              ),
-              10.verticalSpace,
-              // Message List
-              Expanded(
-                child: state.allChatList.isEmpty
-                    ? Center(
-                  child: Text(
-                    'Chats will show here',
-                    style: AppTextStyles.textStylePoppinsMedium.copyWith(
-                      color: AppColors.colorPrimaryAlpha,
-                      fontSize: 10.sp,
-                    ),
+                20.verticalSpace,
+                Text(
+                  'Message List',
+                  style: AppTextStyles.textStylePoppinsMedium.copyWith(
+                    color: AppColors.colorPrimary,
+                    fontSize: 13.sp,
                   ),
-                )
-                    : ListView.builder(
-                  itemCount: state.allChatList.length,
-                  itemBuilder: (context, index) {
-                    final chat = state.allChatList[index];
-                    return GestureDetector(
-                      onTap: () {
-                        final senderId = chat.senderID;
-                        final receiverId = chat.receiverID;
-                        final chatId = generateChatId(senderId, receiverId);
-                        stateNotifier.selectChat(chatId);
-                        final chatNotifier = ref.read(chatNotifierProvider.notifier);
-                        chatNotifier.sendPeopleId(chat.senderID);
-                        AutoRouter.of(context).push(DirectMessageRoute(peopleId: chat.receiverID));
-                      },
-                      child: ListTile(
-                        contentPadding: EdgeInsets.symmetric(vertical: 1.h),
-                        leading: ClipRRect(
-                          borderRadius: BorderRadius.circular(50),
-                          child: SizedBox(
-                            width: 45.w,
-                            height: 45.h,
-                            child: CachedNetworkImage(
-                              imageUrl: Assets.avatar,
-                              placeholder: (context, url) => const CircularProgressIndicator(color: AppColors.colorPrimary),
-                              errorWidget: (context, url, error) => ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: Image.asset(
-                                  Assets.profileImage,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              imageBuilder: (context, imageProvider) => Container(
-                                width: 45.w,
-                                height: 45.h,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  image: DecorationImage(
-                                    image: imageProvider,
+                ),
+                4.verticalSpace,
+                Text(
+                  state.allChatList.length.toString(),
+                  style: AppTextStyles.textStylePoppinsRegular.copyWith(
+                    color: AppColors.colorPrimaryAlpha,
+                    fontSize: 10.sp,
+                  ),
+                ),
+                10.verticalSpace,
+                // Message List
+                Expanded(
+                  child: state.allChatList.isEmpty
+                      ? Center(
+                    child: Text(
+                      'Chats will show here',
+                      style: AppTextStyles.textStylePoppinsMedium.copyWith(
+                        color: AppColors.colorPrimaryAlpha,
+                        fontSize: 10.sp,
+                      ),
+                    ),
+                  )
+                      : ListView.builder(
+                    itemCount: state.allChatList.length,
+                    itemBuilder: (context, index) {
+                      final chat = state.allChatList[index];
+                      return GestureDetector(
+                        onTap: () {
+                          final senderId = chat.senderID;
+                          final receiverId = chat.receiverID;
+                          final chatId = generateChatId(senderId, receiverId);
+                          stateNotifier.selectChat(chatId);
+                          final chatNotifier = ref.read(chatNotifierProvider.notifier);
+                          chatNotifier.sendPeopleId(chat.senderID);
+                          AutoRouter.of(context).push(DirectMessageRoute(peopleId: chat.receiverID));
+                        },
+                        child: ListTile(
+                          contentPadding: EdgeInsets.symmetric(vertical: 1.h),
+                          leading: ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: SizedBox(
+                              width: 45.w,
+                              height: 45.h,
+                              child: CachedNetworkImage(
+                                imageUrl: Assets.avatar,
+                                placeholder: (context, url) => const CircularProgressIndicator(color: AppColors.colorPrimary),
+                                errorWidget: (context, url, error) => ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.asset(
+                                    Assets.profileImage,
                                     fit: BoxFit.cover,
                                   ),
                                 ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        title: Text(
-                          'User Name $index',
-                          style: AppTextStyles.textStylePoppinsMedium.copyWith(
-                            color: AppColors.colorPrimary,
-                            fontSize: 13.sp,
-                          ),
-                        ),
-                        subtitle: Text(
-                          'Lorem ipsum dolor sit amet consectetur.',
-                          style: AppTextStyles.textStylePoppinsRegular.copyWith(
-                            color: AppColors.colorPrimaryAlpha,
-                            fontSize: 10.sp,
-                          ),
-                          maxLines: 1,
-                        ),
-                        trailing: Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Feb 28, 2024',
-                              style: AppTextStyles.textStylePoppinsRegular.copyWith(
-                                fontSize: 10.sp,
-                                color: AppColors.colorText3,
-                              ),
-                            ),
-                            4.verticalSpace,
-                            Container(
-                              width: 16.w,
-                              height: 16.h,
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.black,
-                              ),
-                              child: Center(
-                                child: Text(
-                                  '10+',
-                                  style: AppTextStyles.textStylePoppinsRegular.copyWith(
-                                    color: AppColors.colorWhite,
-                                    fontSize: 8.sp,
+                                imageBuilder: (context, imageProvider) => Container(
+                                  width: 45.w,
+                                  height: 45.h,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    image: DecorationImage(
+                                      image: imageProvider,
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
-                          ],
-                        ),
-                        onTap: () {
-                          AutoRouter.of(context).push(DirectMessageRoute(peopleId: ''));
+                          ),
+                          title: Text(
+                            'User Name $index',
+                            style: AppTextStyles.textStylePoppinsMedium.copyWith(
+                              color: AppColors.colorPrimary,
+                              fontSize: 13.sp,
+                            ),
+                          ),
+                          subtitle: Text(
+                            'Lorem ipsum dolor sit amet consectetur.',
+                            style: AppTextStyles.textStylePoppinsRegular.copyWith(
+                              color: AppColors.colorPrimaryAlpha,
+                              fontSize: 10.sp,
+                            ),
+                            maxLines: 1,
+                          ),
+                          trailing: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Feb 28, 2024',
+                                style: AppTextStyles.textStylePoppinsRegular.copyWith(
+                                  fontSize: 10.sp,
+                                  color: AppColors.colorText3,
+                                ),
+                              ),
+                              4.verticalSpace,
+                              Container(
+                                width: 16.w,
+                                height: 16.h,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.black,
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '10+',
+                                    style: AppTextStyles.textStylePoppinsRegular.copyWith(
+                                      color: AppColors.colorWhite,
+                                      fontSize: 8.sp,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          onTap: () {
+                            AutoRouter.of(context).push(DirectMessageRoute(peopleId: ''));
 
-                        },
-                      ),
-                    );
-                  },
+                          },
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
