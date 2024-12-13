acd9b51170cad2d2e584a62412700071436c43e1
Pinky Prerana <pinkyprerna99@gmail.com>
2024-12-13 13:43:26 +0530

    updated app with old logo

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -1,7 +1,7 @@
 class Assets {
-  static const String logo = 'assets/images/logo.png';
-  static const String logoTransparent = 'assets/images/logo-transparent.png';
-  static const String logoFull = 'assets/images/logo.jpg';
+  static const String logo = 'assets/images/logo-full.jpeg';
+  static const String logoTransparent = 'assets/images/logo-transparent-old.png';
+  static const String logoFull = 'assets/images/logo-full.jpeg';
   //
   static const String landingintro = 'assets/images/landing-intro.png';
   //
